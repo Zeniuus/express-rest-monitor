982042c6a0eef82e8496181cf3e8c7639e716dbe
Zeniuus <jsh56son@kaist.ac.kr>
2018-04-25 17:44:59 +0900

    Implement basic functions of sending new request

--- examples/basic.js
+++ examples/basic.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressRestMonitor = require('../index');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = 3000;
@@ -7,6 +8,8 @@ const port = 3000;
 /* TODO: Move other usage of middlewares into expressRestMonitor */
 app.use(expressRestMonitor());
 app.use(express.static(`${__dirname}/../lib/static`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', `${__dirname}/../lib/templates`);
 app.engine('html', require('ejs').renderFile);
 
@@ -15,6 +18,24 @@ app.get('/', (req, res) => {
     a: 1,
     b: 2,
     c: 3,
+  });
+});
+
+app.post('/', (req, res) => {
+  res.json({
+    body: req.body,
+  });
+});
+
+app.put('/', (req, res) => {
+  res.json({
+    method: 'PUT',
+  });
+});
+
+app.delete('/', (req, res) => {
+  res.json({
+    method: 'DELETE',
   });
 });
 
